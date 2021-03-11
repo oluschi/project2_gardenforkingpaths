420b8dea62ff3231a868fa1e8d752217fecd3d23
oluschi <72060680+oluschi@users.noreply.github.com>
2021-03-11 14:48:29 -0500

    Add files via upload

---
+++ pages/myscripts1.js
@@ -0,0 +1,23 @@
+$('#slide').on('click', function() {
+  $(this).toggleClass('clicked');
+});
+
+$('#slide1').on('click', function() {
+  $(this).toggleClass('clicked');
+});
+
+$('#slide2').on('click', function() {
+  $(this).toggleClass('clicked');
+});
+
+$('#slide3').on('click', function() {
+  $(this).toggleClass('clicked');
+});
+
+$('#slide4').on('click', function() {
+  $(this).toggleClass('clicked');
+});
+
+$('#slide5').on('click', function() {
+  $(this).toggleClass('clicked');
+});
